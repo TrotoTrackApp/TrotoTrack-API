875db7b2d3af623587fcebd5b8e8a15ececb1c17
Al Hilaluddin <shiigetsu1@gmail.com>
2024-05-24 16:25:32 +0800

    feat: update all response http

--- utils/constanta/constanta.js
+++ utils/constanta/constanta.js
@@ -1,10 +1,17 @@
-// Successfully
 const message = {
+    // Success message
     SUCCESS_CREATED: "Success Created Data",
     SUCCESS_UPDATED: "Success Updated Data",
     SUCCESS_DELETED: "Success Deleted Data",
     SUCCESS_GET    : "Success Get Data",
-    SUCCESS_GET_ALL: "Success Get All Data"
+    SUCCESS_GET_ALL: "Success Get All Data",
+
+    // Error message
+    ERROR_INTERNAL_SERVER: "Internal server error",
+    ERROR_NOT_FOUND      : "Data not found",
+    ERROR_UNAUTHORIZED   : "Unauthorized access",
+    ERROR_FORBIDDEN      : "Forbidden access",
+    ERROR_BAD_REQUEST    : "Bad request",
 };
 
 module.exports = { message };
