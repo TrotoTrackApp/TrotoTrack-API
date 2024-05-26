4d1dccb7a746657c3521f615c6c0150b46edc0f7
exoream <shiigetsu1@gmail.com>
2024-05-26 16:45:27 +0800

    feat: add model and migration article

--- app/migrate/migrate.js
+++ app/migrate/migrate.js
@@ -1,7 +1,9 @@
 const User = require("../../feature/user/model/model");
+const Article = require("../../feature/article/model/model");
 
 async function autoMigrate() {
   await User.sync();
+  await Article.sync();
   console.log("Auto migration successful");
 }
 
