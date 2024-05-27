d52f0720194d122f8c15452bc7f88cce4e141e23
exoream <shiigetsu1@gmail.com>
2024-05-27 09:32:16 +0800

    feat: add entity data

---
+++ feature/report/entity/interface.js
@@ -0,0 +1,43 @@
+class ReportRepositoryInterface {
+  async createReport(data, file) {
+    throw new Error("Method not implemented");
+  }
+
+  async updateReport(id, data, file) {
+    throw new Error("Method not implemented");
+  }
+
+  async deleteReport(id) {
+    throw new Error("Method not implemented");
+  }
+
+  async getReportById(id) {
+    throw new Error("Method not implemented");
+  }
+
+  async getAllReport() {
+    throw new Error("Method not implemented");
+  }
+}
+
+class ReportServiceInterface {
+  async createReport(data, file) {
+    throw new Error("Method not implemented");
+  }
+
+  async updateReport(id, data, file) {
+    throw new Error("Method not implemented");
+  }
+
+  async deleteReport(id) {
+    throw new Error("Method not implemented");
+  }
+
+  async getReportById(id) {
+    throw new Error("Method not implemented");
+  }
+
+  async getAllReport() {
+    throw new Error("Method not implemented");
+  }
+}
