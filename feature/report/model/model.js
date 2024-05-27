446d2534e83e4236129c397d7856fc9ab54cc889
exoream <shiigetsu1@gmail.com>
2024-05-27 09:17:23 +0800

    feat: add model report

---
+++ feature/report/model/model.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../../app/database/mysql");
+
+const Report = sequelize.define(
+  "Report",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reference_location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: true,
+    paranoid: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    deletedAt: "deleted_at",
+  }
+);
+
+module.exports = Report;
