7fe4e72b9a32687f511a3b1fad4a87e2c9b585f9
exoream <shiigetsu1@gmail.com>
2024-05-27 09:18:57 +0800

    feat: add migration report

--- app/migrate/migrate.js
+++ app/migrate/migrate.js
@@ -1,7 +1,9 @@
 const User = require("../../feature/user/model/model");
+const Report = require("../../feature/report/model/model");
 
 async function autoMigrate() {
   await User.sync();
+  await Report.sync();
   console.log("Auto migration successful");
 }
 
