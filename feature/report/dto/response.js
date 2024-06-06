a7eaa1a96afb19ee351a3c8b955958c8188e1ef6
exoream <shiigetsu1@gmail.com>
2024-06-06 13:18:25 +0800

    feat: update response get all reports

--- feature/report/dto/response.js
+++ feature/report/dto/response.js
@@ -23,12 +23,8 @@ function reportListResponse(reportlist) {
     return {
       id: report.id,
       location: report.location,
-      reference_location: report.referenceLocation,
-      latitude: report.latitude,
-      longitude: report.longitude,
       image: report.image,
       status_damage: report.statusDamage,
-      description: report.description,
       status: report.status,
       like: report.like,
       created_at: report.createdAt,
