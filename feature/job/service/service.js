987f5a518457961040eca483c8523adf16d681a3
exoream <shiigetsu1@gmail.com>
2024-06-15 21:23:33 +0800

    feat: add service job

--- feature/job/service/service.js
+++ feature/job/service/service.js
@@ -42,4 +42,92 @@ class JobService extends JobServicesInterface {
     const job = await this.jobRepo.createJob(data, file);
     return job;
   }
+
+  async getJobById(id) {
+    if (!id) {
+      throw new ValidationError("Id is required");
+    }
+
+    if (!validator.isUUID(id)) {
+      throw new ValidationError(message.ERROT_ID_INVALID);
+    }
+
+    const job = await this.jobRepo.getJobById(id);
+    return job;
+  }
+
+  async getAllJob() {
+    const jobs = await this.jobRepo.getAllJob();
+    if (article.length === 0) {
+      throw new ValidationError("No article found");
+    }
+    return jobs;
+  }
+
+  async updateJobById(id, updatedData, file) {
+    if (!id) {
+      throw new ValidationError("Id is required");
+    }
+
+    if (!validator.isUUID(id)) {
+      throw new ValidationError(message.ERROT_ID_INVALID);
+    }
+
+    const job = await this.jobRepo.getJobById(id);
+    if (!job) {
+      throw new NotFoundError("Job not found");
+    }
+
+    // Validate NIK if provided
+    if (updatedData.nik) {
+      if (updatedData.nik.length < 16) {
+        throw new ValidationError("NIK must be at least 16 characters long");
+      }
+
+      const nikExist = await this.jobRepo.getJobByNik(updatedData.nik);
+      if (nikExist && nikExist.id !== id) {
+        throw new ValidationError("NIK already exists for another job");
+      }
+    }
+
+    // Validate phone number if provided
+    if (updatedData.phone) {
+      if (updatedData.phone.length < 10) {
+        throw new ValidationError("Phone must be at least 10 characters long");
+      }
+    }
+
+    // Validate file if provided
+    if (file) {
+      if (file.size > 10 * 1024 * 1024) {
+        // File size should not exceed 10MB
+        throw new ValidationError("File size must not be greater than 10MB");
+      }
+
+      const allowedFileTypes = ["application/pdf"];
+      if (!allowedFileTypes.includes(file.mimetype)) {
+        // File must be a PDF
+        throw new ValidationError("Invalid file type. Only PDF is allowed.");
+      }
+    }
+
+    const updatedJob = await this.jobRepo.updateJobById(id, updatedData, file);
+    return updatedJob;
+  }
+
+  async deleteJobById(id) {
+    if (!id) {
+      throw new ValidationError("Id is required");
+    }
+
+    if (!validator.isUUID(id)) {
+      throw new ValidationError(message.ERROT_ID_INVALID);
+    }
+
+    const deletedJob = await this.jobRepo.deleteJobById(id);
+    if (!deletedJob) {
+      throw new NotFoundError("Job not found");
+    }
+    return deletedJob;
+  }
 }
